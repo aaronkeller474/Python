c436eee3b36595461f550d701724e2bf35807843
Aaron Keller <aaron@connectio.us>
2016-08-04 14:53:13 -0700

    Python GUI

--- GUI.py
+++ GUI.py
@@ -1,19 +1,23 @@
 from Tkinter import *
 from os import system
+from datetime import *
+import speech_recognition
 
 root = Tk()
 
 def sayName(self):
+    today = datetime.today()
     self.fntext = fnameEntry.get()
     self.lntext = lnameEntry.get()
-    system('say Hello ' + self.fntext + self.lntext + ' . ' ' How may I help you sir?')
+    system('say Hello ' + self.fntext + self.lntext + ' . ')
+    system('say The current date is: ' + str(today))
 
 #Creates the Labels/Entry/Buttons Labels
 fname = Label(root, text='First Name')
 lname = Label(root, text='Last Name')
 fnameEntry = Entry(root)
 lnameEntry = Entry(root)
-btnClick = Button(root, text='Talk to me')
+btnClick = Button(root, text='Talk to me!')
 
 #Puts the Labels/Entry/Buttons in the Window
 fname.grid(row=0)
